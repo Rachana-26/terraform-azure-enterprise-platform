2daf322a27e30bc29b430092f59d9526927cf006
Rachana-26 <rachana06racchu@gmail.com>
2026-06-22 13:48:04 +0530

    Add remote backend configuration

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstateab2026"
+    container_name       = "tfstate"
+    key                  = "enterprise-platform-prod.tfstate"
+  }
+}
